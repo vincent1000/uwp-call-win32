237476bf6b6f12fea5234be66e3e2977cd3ac15b
vincent1000 <41051215+vincent1000@users.noreply.github.com>
2020-07-02 17:48:04 +0800

    Add files via upload

---
+++ dotnetCoreApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace dotnetCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Process.Start("ms-settings:");
+            Console.WriteLine("HELLO");
+            while (true) ;
+        }
+    }
+}
